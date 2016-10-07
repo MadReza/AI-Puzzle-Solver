ae246b2850698d923d5b710b08892dde6ea43244
Ahmad Madabadi <a_madab@encs.concordia.ca>
2016-10-07 16:25:38 -0400

    Added BFS implementation, needs cleaning

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from classes.Helper import getCmdArgs
 from classes.Puzzle import Puzzle
 from classes.Solver import Solver
 
-def dfs_path_show(start, end, path):
+def path_show(start, end, path):
     current = end
     previous = path      #As this is a dictionary with previous caller
     path = [current]
@@ -24,11 +24,12 @@ if __name__ == '__main__':
     puzzle = Puzzle(args.thePuzzle)
     solver = Solver(puzzle, args.algorithm)
     solver.solve()
-    dfs_solution = solver.dfs()
-
+    #dfs_solution = solver.dfs()
+    bfs_solution = solver.bfs()
+    
     solved_puzzle = Puzzle([1,2,3,4,5,6,7,8,0])
     initial_puzzle = puzzle
-    dfs_path_show(initial_puzzle, solved_puzzle, dfs_solution)
+    #path_show(initial_puzzle, solved_puzzle, dfs_solution)
 
 
 
