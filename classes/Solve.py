af37e167524955815447761ae41315e648c4d4ec
Reza Madabadi <rezmaza@gmail.com>
2016-10-07 10:38:36 -0400

    Started Solver, need to switch pc

---
+++ classes/Solve.py
@@ -0,0 +1,11 @@
+from Puzzle import Puzzle
+
+class Solver:
+    algorithm=""
+    puzzle =null
+
+    def __init__(self, puzzle, algorithm="a*"):
+        self.algorithm = algorithm
+        self.puzzle = puzzle
+
+        
--- main.py
+++ main.py
@@ -1,31 +1,10 @@
 from classes.Helper import getCmdArgs
 from classes.Puzzle import Puzzle
+from classes.Solver import Solver
 
 if __name__ == '__main__':
     args = getCmdArgs()
-    print args.algorithm
-    print type(args.thePuzzle[0])
     puzzle = Puzzle(args.thePuzzle)
-    print puzzle.possible_moves()
-    print hash(puzzle)
-    print "---------"
-    x = puzzle.move(1)
-    print x
-    print hash(x)
-    print "---------"
-    x = puzzle.move(3)
-    print x
-    print hash(x)
-    print "---------"
-    x = puzzle.move(5)
-    print x
-    print hash(x)
-    print "---------"
-    x = puzzle.move(7)
-    print x
-    print hash(x)
-    print "---------"
-    x = puzzle.move(2)
-    print x
-    print hash(x)
+    solver = Solver(puzzle, args.algorithm)
+
 
